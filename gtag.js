81ed0884aa676fda1f140de5e7e82ed357217305
areenkun <101259369+areenkun@users.noreply.github.com>
2024-01-24 07:45:59 +0700

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Global site tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-P2KPWWWN8F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-P2KPWWWN8F');
+</script>
